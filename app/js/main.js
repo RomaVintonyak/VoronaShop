c2c73e46a604e262d62c15a2777067b772aed44c
RomaVintonyak <19roman23@gmail.com>
2022-06-23 18:32:54 +0300

    create header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,19 @@
 jQuery(document).ready(function () {
   "use script";
- 
-  var btn = $("._callback");
-  btn.on("click", function(){
-    alert("Its work");
-    prompt();
+  //burger btn
+  var burgerBtn = $(".burger__btn");
+  burgerBtn.on("click", function(event){
+    event.preventDefault();
+    $(this).toggleClass("burger__btn--open");
+  });
+  //header search form
+  var searchBtn = $("._searchBtn");
+  var searchForm = $(".header__search");
+  var closeSearchBtn = $("._closeSearch");
+  searchBtn.on("click", function(){
+    searchForm.addClass("header__search--show");
+  });
+  closeSearchBtn.on("click", function(){
+    searchForm.removeClass("header__search--show");
   });
 });
--- app/js_src/main.js
+++ app/js_src/main.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function () {
   "use script";
+  //burger btn
+  var burgerBtn = $(".burger__btn");
+  burgerBtn.on("click", function(event){
+    event.preventDefault();
+    $(this).toggleClass("burger__btn--open");
+  });
+  //header search form
+  var searchBtn = $("._searchBtn");
+  var searchForm = $(".header__search");
+  var closeSearchBtn = $("._closeSearch");
+  searchBtn.on("click", function(){
+    searchForm.addClass("header__search--show");
+  });
+  closeSearchBtn.on("click", function(){
+    searchForm.removeClass("header__search--show");
+  });
 });
